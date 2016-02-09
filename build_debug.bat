b24252513b1bbe9599a6a305f58efb07117ff2cc
Trylobot <trylobot@gmail.com>
2016-02-08 22:04:45 -0600

    added linker opts

--- build_debug.bat
+++ build_debug.bat
@@ -1,3 +1,4 @@
 del /F sf-ship-ed.debug.exe
+set "BMK_LD_OPTS=-lmsvcrt -lgcc"
 bmk makeapp -a -o sf-ship-ed.debug.exe sf-ship-ed.bmx
 pause
--- build_release.bat
+++ build_release.bat
@@ -1,4 +1,5 @@
 del /F sf-ship-ed.exe
+set "BMK_LD_OPTS=-lmsvcrt -lgcc"
 bmk makeapp -a -r -t gui -o sf-ship-ed.exe sf-ship-ed.bmx
 upx sf-ship-ed.exe
 pause
